c7b388061e20ba33bfe2c01938b4a21eef3ec42e
KingFrodo <foro.lukas@liwest.at>
2019-03-10 17:34:45 +0100

    first searchView

--- app/src/main/java/com/example/simplecarlist/MainActivity.java
+++ app/src/main/java/com/example/simplecarlist/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -18,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     File file = new File("cars.csv");
     ListView listView;
     List<Car> cars = new ArrayList<>();
+    List<String> firstNames = new ArrayList<>();
+    List<String> lastNames = new ArrayList<>();
+    List<String> brands = new ArrayList<>();
+    List<String> models = new ArrayList<>();
+    List<String> carStrings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +33,35 @@ public class MainActivity extends AppCompatActivity {
 
         listView = (ListView) findViewById(R.id.listView);
         readAssets();
+        for (int i = 0; i < cars.size(); i++) {
+            carStrings.add(cars.get(i).getFirstName() + cars.get(i).getLastName() + cars.get(i).getBrand() + cars.get(i).getModel());
+        }
 
-        MyAdapter myAdapter = new MyAdapter(this, R.layout.list_view_layout, cars);
+        final MyAdapter myAdapter = new MyAdapter(this, R.layout.list_view_layout, cars);
         listView.setAdapter(myAdapter);
-    }
+
+        SearchView searchView = (SearchView) findViewById(R.id.searchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                if (carStrings.contains(query)) {
+                    myAdapter.getFilter().filter(query);
+                } else {
+                    Toast.makeText(MainActivity.this, "No Match found", Toast.LENGTH_LONG).show();
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                myAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        }
 
     private InputStream getInputStreamForAsset(String filename){
         AssetManager assets = getAssets();
@@ -56,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
                 String lastName = split[2];
                 String brand = split[11];
                 String model = split[12];
-                
+
                 Car car = new Car(firstName, lastName, brand, model);
                 cars.add(car);
             }
